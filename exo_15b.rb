12ec5c0aa85e8f8bddb5cc8a9eb643e946e42b7f
Theo <theo.gianella@proton.me>
2023-01-16 18:30:25 +0100

    Adding alternate versions of exercices 15 to 17 (with nested loops)

---
+++ exo_15b.rb
@@ -0,0 +1,12 @@
+puts "Donne moi un nombre entre 1 et 25"
+nb = gets.chomp.to_i
+if nb >= 1 and nb <= 25 then
+  nb.times do |i|
+    (i+1).times do 
+      print "#"
+    end
+    puts
+  end
+else
+  puts "Ce n'est pas un nombre entre 1 et 25 !"
+end
---
+++ exo_17b.rb
@@ -0,0 +1,18 @@
+puts "Donne moi un nombre entre 1 et 25"
+nb = gets.chomp.to_i
+if nb >= 1 and nb <= 25 then
+  nb.times do |i|
+    (nb-(i+1)).times do
+      print " "
+    end
+    ((2*i)+1).times do
+      print "#"
+    end
+    (nb-(i+1)).times do
+      print " "
+    end
+    puts
+  end
+else
+  puts "Ce n'est pas un nombre entre 1 et 25 !"
+end
